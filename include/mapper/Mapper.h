90b605e4cfa1b0562bd13aebafb9d196b31a2750
Bahn <Bjoern.Bahn@dlr.de>
2020-04-29 13:28:41 +0200

    add yaml config template decoding

--- include/mapper/Mapper.h
+++ include/mapper/Mapper.h
@@ -1,42 +1,67 @@
 #ifndef MAPPER_H
 #define MAPPER_H
 
+#include <cctype>
 #include <string>
-#include <vector>
+#include <map>
 #include <variant>
-#include "../simulation_interfaces/iSimulationData.h"
-#include "../simulation_interfaces/internalState.h"
+#include <iostream>
+#include "yaml-cpp/yaml.h"
+#include "simulation_interfaces/iSimulationData.h"
+#include "simulation_interfaces/internalState.h"
+#include "configreader/StandardYAMLConfig.h"
 
 typedef std::variant<int, float, double, bool, std::string> values_t;
 
+enum DataTypes
+{
+	INTEGER,
+	FLOAT,
+	DOUBLE,
+	BOOL,
+	STRING,
+	ERROR
+};
+
+struct interfaceNameAndIndex {
+	std::string interfaceName;
+	int index;
+};
+
 class MapperConfig {
 public:
-	std::vector<std::string> intInputVector;
-	std::vector<std::string> intOutputVector;
-	std::vector<std::string> floatInputVector;
-	std::vector<std::string> floatOutputVector;
-	std::vector<std::string> doubleInputVector;
-	std::vector<std::string> doubleOutputVector;
-	std::vector<std::string> boolInputVector;
-	std::vector<std::string> boolOutputVector;
-	std::vector<std::string> stringInputVector;
-	std::vector<std::string> stringOutputVector;
+	//base_names to interface_names
+	std::map < std::string, interfaceNameAndIndex> intInputMap;
+	std::map<std::string, interfaceNameAndIndex> intOutputMap;
+	std::map<std::string, interfaceNameAndIndex> floatInputMap;
+	std::map<std::string, interfaceNameAndIndex> floatOutputMap;
+	std::map<std::string, interfaceNameAndIndex> doubleInputMap;
+	std::map<std::string, interfaceNameAndIndex> doubleOutputMap;
+	std::map<std::string, interfaceNameAndIndex> boolInputMap;
+	std::map<std::string, interfaceNameAndIndex> boolOutputMap;
+	std::map<std::string, interfaceNameAndIndex> stringInputMap;
+	std::map<std::string, interfaceNameAndIndex> stringOutputMap;
 };
 
 class Mapper {
 
 private:
 	MapperConfig config;
+protected:
+	//most interfaces need these
+	std::string ip;
+	int port;
 
 public:
 	int extractInternalState(internalState* pointer);
-	int readConfiguration(std::string config);
+	int readConfiguration(std::string path, std::string interfaceName);
 
 private:
 	void filterInput(Mapper* outputMapper, internalState* state);
 
 protected:
-	virtual void mapTo(values_t value, std::string name, std::string type) = 0;
+	virtual void mapTo(values_t value, std::string name, DataTypes type) = 0;
+	DataTypes getType(std::string);
 };
 
 #endif // !MAPPER_H
--- src/Mapper/Mapper.cpp
+++ src/Mapper/Mapper.cpp
@@ -16,95 +16,150 @@ int Mapper::extractInternalState(internalState* state)
 //called from output part of Mapper
 void Mapper::filterInput(Mapper* outputMapper, internalState* foreignState) {
 	//integer
-	for (unsigned int index = 0; index < outputMapper->config.intOutputVector.size(); index++) {
-		for (std::string name : config.intInputVector) {
-			if (name.compare(config.intOutputVector.at(index))) {
+	for (std::pair<std::string, interfaceNameAndIndex> output : outputMapper->config.intOutputMap) {
+		for (std::pair<std::string, interfaceNameAndIndex> input : config.intInputMap) {
+			if (input.first.compare(output.first)) {
 				//each Mapper implementation handles this
-				mapTo(foreignState->integers.at(index), name, "integer");
+				mapTo(foreignState->integers.at(output.second.index), input.second.interfaceName, INTEGER);
 			}
 		}
 	}
 	//float
-	for (unsigned int index = 0; index < outputMapper->config.floatOutputVector.size(); index++) {
-		for (std::string name : config.floatInputVector) {
-			if (name.compare(config.floatOutputVector.at(index))) {
+	for (std::pair<std::string, interfaceNameAndIndex> output : outputMapper->config.floatOutputMap) {
+		for (std::pair<std::string, interfaceNameAndIndex> input : config.floatInputMap) {
+			if (input.first.compare(output.first)) {
 				//each Mapper implementation handles this
-				mapTo(foreignState->floats.at(index), name, "float");
+				mapTo(foreignState->floats.at(output.second.index), input.second.interfaceName, FLOAT);
 			}
 		}
 	}
 	//double
-	for (unsigned int index = 0; index < outputMapper->config.doubleOutputVector.size(); index++) {
-		for (std::string name : config.doubleInputVector) {
-			if (name.compare(config.doubleOutputVector.at(index))) {
+	for (std::pair<std::string, interfaceNameAndIndex> output : outputMapper->config.doubleOutputMap) {
+		for (std::pair<std::string, interfaceNameAndIndex> input : config.doubleInputMap) {
+			if (input.first.compare(output.first)) {
 				//each Mapper implementation handles this
-				mapTo(foreignState->doubles.at(index), name, "double");
+				mapTo(foreignState->doubles.at(output.second.index), input.second.interfaceName, DOUBLE);
 			}
 		}
 	}
 	//bool
-	for (unsigned int index = 0; index < outputMapper->config.boolOutputVector.size(); index++) {
-		for (std::string name : config.boolInputVector) {
-			if (name.compare(config.boolOutputVector.at(index))) {
+	for (std::pair<std::string, interfaceNameAndIndex> output : outputMapper->config.boolOutputMap) {
+		for (std::pair<std::string, interfaceNameAndIndex> input : config.boolInputMap) {
+			if (input.first.compare(output.first)) {
 				//each Mapper implementation handles this
-				mapTo(foreignState->bools.at(index), name, "bool");
+				mapTo(foreignState->bools.at(output.second.index), input.second.interfaceName, BOOL);
 			}
 		}
 	}
 	//string
-	for (unsigned int index = 0; index < outputMapper->config.stringOutputVector.size(); index++) {
-		for (std::string name : config.stringInputVector) {
-			if (name.compare(config.stringOutputVector.at(index))) {
+	for (std::pair<std::string, interfaceNameAndIndex> output : outputMapper->config.stringOutputMap) {
+		for (std::pair<std::string, interfaceNameAndIndex> input : config.stringInputMap) {
+			if (input.first.compare(output.first)) {
 				//each Mapper implementation handles this
-				mapTo(foreignState->strings.at(index), name, "string");
+				mapTo(foreignState->strings.at(output.second.index), input.second.interfaceName, STRING);
 			}
 		}
 	}
 }
 
-int Mapper::readConfiguration(std::string) {
-	//some reading of triples
-	//(interface_name, base_name, type)
+int Mapper::readConfiguration(std::string path, std::string interfaceName) {
 
-	//result
-	//filled input and output vectors
+	YAML::Node simulators = YAML::LoadFile(path);
+	InterfaceYAMLConfig yamlconfig = simulators[interfaceName].as<InterfaceYAMLConfig>();
+
+	port = yamlconfig.port;
+	ip = yamlconfig.ip;
+
+	//fill input and output vectors
+	for (VariableDefinitionMap definition : yamlconfig.input_map) {
+		interfaceNameAndIndex info;
+		switch (getType(definition.type)) {
+		case BOOL:
+			info.index = (int) config.boolInputMap.size();
+			info.interfaceName = definition.interface_name;
+			config.boolInputMap.insert(std::pair<std::string, interfaceNameAndIndex>(definition.base_name, info));
+			break;
+		case INTEGER:
+			info.index = (int) config.intInputMap.size();
+			info.interfaceName = definition.interface_name;
+			config.intInputMap.insert(std::pair<std::string, interfaceNameAndIndex>(definition.base_name, info));
+			break;
+		case FLOAT:
+			info.index = (int) config.floatInputMap.size();
+			info.interfaceName = definition.interface_name;
+			config.floatInputMap.insert(std::pair<std::string, interfaceNameAndIndex>(definition.base_name, info));
+			break;
+		case DOUBLE:
+			info.index = (int) config.doubleInputMap.size();
+			info.interfaceName = definition.interface_name;
+			config.doubleInputMap.insert(std::pair<std::string, interfaceNameAndIndex>(definition.base_name, info));
+			break;
+		case STRING:
+			info.index = (int) config.stringInputMap.size();
+			info.interfaceName = definition.interface_name;
+			config.stringInputMap.insert(std::pair<std::string, interfaceNameAndIndex>(definition.base_name, info));
+			break;
+		case ERROR:
+			std::cout << "Wrong definition of type in input_map of " << interfaceName << " . Allowed are: string, int, (integer), float, double, bool, (boolean)" << std::endl;
+			return 1;
+		}
+
+		for (VariableDefinitionMap definition : yamlconfig.output_map) {
+			interfaceNameAndIndex info;
+			switch (getType(definition.type)) {
+			case BOOL:
+				info.index = (int) config.boolOutputMap.size();
+				info.interfaceName = definition.interface_name;
+				config.boolOutputMap.insert(std::pair<std::string, interfaceNameAndIndex>(definition.base_name, info));
+				break;
+			case INTEGER:
+				info.index = (int) config.intOutputMap.size();
+				info.interfaceName = definition.interface_name;
+				config.intOutputMap.insert(std::pair<std::string, interfaceNameAndIndex>(definition.base_name, info));
+				break;
+			case FLOAT:
+				info.index = (int) config.floatOutputMap.size();
+				info.interfaceName = definition.interface_name;
+				config.floatOutputMap.insert(std::pair<std::string, interfaceNameAndIndex>(definition.base_name, info));
+				break;
+			case DOUBLE:
+				info.index = (int) config.doubleOutputMap.size();
+				info.interfaceName = definition.interface_name;
+				config.doubleOutputMap.insert(std::pair<std::string, interfaceNameAndIndex>(definition.base_name, info));
+				break;
+			case STRING:
+				info.index = (int) config.stringOutputMap.size();
+				info.interfaceName = definition.interface_name;
+				config.stringOutputMap.insert(std::pair<std::string, interfaceNameAndIndex>(definition.base_name, info));
+				break;
+			case ERROR:
+				std::cout << "Wrong definition of type in output_map of " << interfaceName << " . Allowed are: string, int, (integer), float, double, bool, (boolean)" << std::endl;
+				return 1;
+			}
+		}
+	}
 	return 0;
 }
 
-/*
-TDominion mapToDominion(std::string variablename)
-{
-	if (variablename == "MoSAIC.EgoVehicle.Kinematics.Position" && variablename is input) return _input.MoSAIC..... else _output.MoSAIC....
-
-		// alternative
-
-		switch (variablename)
-		{
-		case variablename.equals("MoSAIC.EgoVehicle.Kinematics.Position"):
-			if (input) return _input....;
-			else return _output....;
-			break;
-		}
-
+DataTypes getType(std::string type) {
+	std::transform(type.begin(), type.end(), type.begin(),
+		[](unsigned char c) { return std::tolower(c); });
+	if (type.compare("string") == 0) {
+		return STRING;
+	}
+	else if (type.compare("float") == 0) {
+		return FLOAT;
+	}
+	else if (type.compare("double") == 0) {
+		return DOUBLE;
+	}
+	else if (type.compare("bool") == 0 || type.compare("boolean") == 0) {
+		return BOOL;
+	}
+	else if (type.compare("int") == 0 || type.compare("integer") == 0) {
+		return INTEGER;
+	}
+	else {
+		return ERROR;
+	}
 }
-
-typedef OSI  std::variant<osi3::GroundTruth, osi3::SensorData>
-
-
-// FMI Mapper converts (length, StartPointer, EndPointer) triple into input stream
-OSI mapToFMI(std::string variablename, fstream input(argv[1], ios::in | ios::binary))
-{
-	if (variablename.equals("osi3::GroundTruth") return osi3::GroundTruth.ParseFromIstream(&input)
-
-		// alternative
-
-		switch (variablename)
-		{
-		case variablename.equals("MoSAIC.EgoVehicle.Kinematics.Position"):
-			if (input) return _input....;
-			else return _output....;
-				break;
-		}
-
-}
-*/
