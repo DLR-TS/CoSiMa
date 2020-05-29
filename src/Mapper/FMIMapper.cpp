466b3af7dce91209e04700e21eecf09496fad43c
Nils Wendorff <nils.wendorff@dlr.de>
2020-05-29 12:46:49 +0200

    Add model identifier as prefix to base interface variable names in FMIMapper.cpp

--- src/Mapper/FMIMapper.cpp
+++ src/Mapper/FMIMapper.cpp
@@ -33,7 +33,7 @@ int FMIMapper::readConfiguration(configVariants_t configVariants) {
 
 	auto state = owner.lock()->getInternalState();
 
-	//TODO need basename definitions for base interface
+	//TODO need basename definitions for base interface. Current implementation prepends the modelIdentifier, separated with "."
 	for (const auto& var : *modelDescription->model_variables) {
 		// cannot switch on outputVar.value_type because fmi4cpp uses strings and cannot use getType because type name 'Real' is not defined (maps to some floating point type, typically double) 
 		if (fmi4cpp::fmi2::causality::input == var.causality || fmi4cpp::fmi2::causality::parameter == var.causality) {
@@ -42,7 +42,7 @@ int FMIMapper::readConfiguration(configVariants_t configVariants) {
 			if (var.is_boolean()) {
 				auto boolVar = var.as_boolean();
 				if (boolVar.start().has_value()) {
-					config.boolInputList.push_back(NamesAndIndex(var.name, var.name, (int)state->bools.size()));
+					config.boolInputList.push_back(NamesAndIndex(modelDescription->model_identifier + "." + var.name, var.name, (int)state->bools.size()));
 					state->bools.push_back(boolVar.start().value());
 				}
 				else {
@@ -52,7 +52,7 @@ int FMIMapper::readConfiguration(configVariants_t configVariants) {
 			else if (var.is_enumeration() || var.is_integer()) {
 				auto intVar = var.as_integer();
 				if (intVar.start().has_value()) {
-					config.intInputList.push_back(NamesAndIndex(var.name, var.name, (int)state->integers.size()));
+					config.intInputList.push_back(NamesAndIndex(modelDescription->model_identifier + "." + var.name, var.name, (int)state->integers.size()));
 					state->integers.push_back(intVar.start().value());
 				}
 				else {
@@ -63,12 +63,12 @@ int FMIMapper::readConfiguration(configVariants_t configVariants) {
 				auto realVar = var.as_real();
 				if (realVar.start().has_value()) {
 					if (typeid(fmi2Real) == typeid(double)) {
-						config.doubleInputList.push_back(NamesAndIndex(var.name, var.name, (int)state->doubles.size()));
+						config.doubleInputList.push_back(NamesAndIndex(modelDescription->model_identifier + "." + var.name, var.name, (int)state->doubles.size()));
 						state->doubles.push_back(realVar.start().value());
 
 					}
 					else {
-						config.floatInputList.push_back(NamesAndIndex(var.name, var.name, (int)state->floats.size()));
+						config.floatInputList.push_back(NamesAndIndex(modelDescription->model_identifier + "." + var.name, var.name, (int)state->floats.size()));
 						state->floats.push_back(realVar.start().value());
 					}
 				}
@@ -79,7 +79,7 @@ int FMIMapper::readConfiguration(configVariants_t configVariants) {
 			else /*if (var.is_string())*/ {
 				auto stringVar = var.as_string();
 				if (stringVar.start().has_value()) {
-					config.stringInputList.push_back(NamesAndIndex(var.name, var.name, (int)state->strings.size()));
+					config.stringInputList.push_back(NamesAndIndex(modelDescription->model_identifier + "." + var.name, var.name, (int)state->strings.size()));
 					state->strings.push_back(stringVar.start().value());
 				}
 				else {
@@ -89,25 +89,25 @@ int FMIMapper::readConfiguration(configVariants_t configVariants) {
 		}
 		else if (fmi4cpp::fmi2::causality::output == var.causality || fmi4cpp::fmi2::causality::calculatedParameter == var.causality) {
 			if (var.is_boolean()) {
-				config.boolOutputList.push_back(NamesAndIndex(var.name, var.name, (int)state->bools.size()));
+				config.boolOutputList.push_back(NamesAndIndex(modelDescription->model_identifier + "." + var.name, var.name, (int)state->bools.size()));
 				state->bools.push_back(bool());
 			}
 			else if (var.is_enumeration() || var.is_integer()) {
-				config.intOutputList.push_back(NamesAndIndex(var.name, var.name, (int)state->integers.size()));
+				config.intOutputList.push_back(NamesAndIndex(modelDescription->model_identifier + "." + var.name, var.name, (int)state->integers.size()));
 				state->integers.push_back(int());
 			}
 			else if (var.is_real()) {
 				if (typeid(fmi2Real) == typeid(double)) {
-					config.doubleOutputList.push_back(NamesAndIndex(var.name, var.name, (int)state->doubles.size()));
+					config.doubleOutputList.push_back(NamesAndIndex(modelDescription->model_identifier + "." + var.name, var.name, (int)state->doubles.size()));
 					state->doubles.push_back(double());
 				}
 				else {
-					config.floatOutputList.push_back(NamesAndIndex(var.name, var.name, (int)state->floats.size()));
+					config.floatOutputList.push_back(NamesAndIndex(modelDescription->model_identifier + "." + var.name, var.name, (int)state->floats.size()));
 					state->floats.push_back(float());
 				}
 			}
 			else /*if (var.is_string())*/ {
-				config.stringOutputList.push_back(NamesAndIndex(var.name, var.name, (int)state->strings.size()));
+				config.stringOutputList.push_back(NamesAndIndex(modelDescription->model_identifier + "." + var.name, var.name, (int)state->strings.size()));
 				state->strings.push_back(std::string());
 			}
 		}
