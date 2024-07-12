1d098ccce77e49db6b1ff2693c922a74942ff6a4
Björn Bahn <bjoern.bahn@dlr.de>
2024-07-12 13:21:39 +0200

    linux, with pre-built

--- CMakeModules/FetchSUMO.cmake
+++ CMakeModules/FetchSUMO.cmake
@@ -1,19 +1,12 @@
 cmake_minimum_required(VERSION 3.12)
-include(ExternalProject)
+
+if(NOT DEFINED ENV{SUMO_HOME})
+    message(FATAL_ERROR "SUMO_HOME not found")
+endif()
+
+#Debug print
+message(STATUS "SUMO_HOME = $ENV{SUMO_HOME}")
 
 # Find SUMO library
-if(UNIX)
-	find_path(SUMO_INCLUDE_DIR NAMES "sumo.h" PATHS "/usr/include/sumo" "/usr/local/include/sumo")
-	find_library(SUMO_LIBRARY NAMES "sumo" PATHS "/usr/lib" "/usr/local/lib")
-endif(UNIX)
-if(WIN32)
-	find_path(SUMO_INCLUDE_DIR NAMES "libsumo/libsumo.h" PATHS "C:/Program Files (x86)/Eclipse/Sumo/include")
-	find_library(SUMO_LIBRARY NAMES "libsumocpp.lib" PATHS "C:/Program Files (x86)/Eclipse/Sumo/bin")
-endif()
-
-if(NOT SUMO_INCLUDE_DIR OR NOT SUMO_LIBRARY)
-    message(FATAL_ERROR "SUMO library not found")
-endif()
-
-# Include SUMO headers
-include_directories(${SUMO_INCLUDE_DIR})
+include_directories("$ENV{SUMO_HOME}/src")
+link_directories("$ENV{SUMO_HOME}/bin")
